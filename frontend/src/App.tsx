9fa61d42076cb8524ece213f0fb54dc93a7798d4
Gurpreet Singh Laller <laller@outlook.com>
2023-03-07 15:23:22 +0100

    added logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,7 @@
-import { useContext, useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import useAuth from './hooks/useAuth'
 import IdCard from './components/IdCard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
 
   return (
